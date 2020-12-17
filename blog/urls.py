fcadf9d6e1de86b2281ba86178bae7183ad07db7
Ryan Pilon <ryanpilon1995@gmail.com>
2020-12-16 17:32:21 -0800

    finished lab 27

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, BlogDetailView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
+    path('detail/<int:pk>', BlogDetailView.as_view(), name="blog_detail"),
 ]
